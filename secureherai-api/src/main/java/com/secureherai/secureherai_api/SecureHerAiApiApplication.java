a1077e45f7e0b3e861bc28cea66b2d627b52308f
AN-SWAPNIL <a.n.swapnil2003@gmail.com>
2025-05-26 16:21:44 +0600

    initial backend setup

---
+++ secureherai-api/src/main/java/com/secureherai/secureherai_api/SecureHerAiApiApplication.java
@@ -0,0 +1,13 @@
+package com.secureherai.secureherai_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureHerAiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureHerAiApiApplication.class, args);
+	}
+
+}
---
+++ secureherai-api/src/test/java/com/secureherai/secureherai_api/SecureHerAiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureherai.secureherai_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureHerAiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
